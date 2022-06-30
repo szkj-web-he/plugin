6d15daf403938a9372d01bb7a11a664eb8c0797f
szkj-web-he <83932083+szkj-web-he@users.noreply.github.com>
2022-06-30 09:40:09 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -31,7 +31,6 @@ const Main: React.FC = () => {
                 data[state[i].code][col.code] = col.value ?? 0;
             }
         }
-        console.log(JSON.stringify(data));
         comms.state = data;
     }, [state]);
 
